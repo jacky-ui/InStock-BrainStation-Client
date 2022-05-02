67b2b6332d4ccdfa581e0f117ee02d780efee846
Cyril Hahn <cyrilhahn@gmail.com>
2022-05-02 14:23:48 -0700

    router dom and routes created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.scss';
+import "./App.scss";
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/warehouse/:warehouseId" component={Warehouse} />
+          <Route
+            path="/editWarehouse/:editWarehouseId"
+            component={EditWarehouse}
+          />
+          <Route path="/addWarehouse" component={AddWarehouse} />
+          <Route path="/inventory" component={Inventory} />
+          <Route path="/item" component={Item} />
+          <Route path="/editItem/editItemId" component={EditItem} />
+          <Route path="/addItem/addItemId" component={AddItem} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
