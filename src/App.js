346303ac32379d277505f6b273bf21fe72a96ef3
Cyril Hahn <cyrilhahn@gmail.com>
2022-05-03 15:44:30 -0700

    title, searchbar, button added. responsive.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Header />
-        {/* <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/warehouse/:warehouseId" component={Warehouse} />
+        <Switch>
+          <Route path="/" exact component={WarehousePage}/>
+          {/* <Route path="/warehouse/:warehouseId" component={Warehouse} />
           <Route
             path="/warehouse/:warehouseId/edit"
             component={EditWarehouse}
@@ -20,8 +21,8 @@ function App() {
           <Route path="/inventory" component={Inventory} />
           <Route path="/inventory/:itemId" component={Item} />
           <Route path="/inventory/:itemId/edit" component={EditItem} />
-          <Route path="/inventory/add" component={AddItem} />
-        </Switch> */}
+          <Route path="/inventory/add" component={AddItem} /> */}
+        </Switch>
         <Footer />
       </BrowserRouter>
     </div>
--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -2,20 +2,36 @@ import "./WarehousePage.scss";
 import React, { Component } from "react";
 
 class WarehousePage extends Component {
-    state = {
-        warehousesData: []
-    };
-    // A function to fetch Warehouses Data from Warehouses API Route (/warehouses)
+  state = {
+    warehousesData: [],
+  };
+  // A function to fetch Warehouses Data from Warehouses API Route (/warehouses)
 
-    // Below use Component Lifecycle Methods such as componentDidMount to grab data
+  // Below use Component Lifecycle Methods such as componentDidMount to grab data
 
-    render() {
-        return (
-            <div>
-
+  render() {
+    return (
+      <div className="warehouses">
+        <div className="warehouses__title-container-wrapper">
+          <div className="warehouses__title-container">
+            <h1 className="warehouses__title">Warehouses</h1>
+            <div className="warhouses__search-btn-container">
+              <input
+                type="search"
+                name=""
+                id=""
+                className="warehouses__search-bar"
+                placeholder="Search..."
+              />
+              <button className="warehouses__btn-add">
+                + Add New Warehouse
+              </button>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WarehousePage;
