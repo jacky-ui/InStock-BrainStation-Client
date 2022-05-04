ee4d39e8b77d50ca94e35322dbac88225fd80bf3
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-04 02:02:29 -0400

    Added a couple components that can be reusable

---
+++ src/components/WarehouseForm/WarehouseForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function WarehouseForm({ warehouse }) {
+    return (
+        <section>
+            <form>
+                <div className='form-left'>
+                    <h2 className='form__title'></h2>
+                </div>
+                <div className='form-right'>
+                    <h2 className='form__title'>Contact Details</h2>
+
+                </div>
+                <div className='form-btns'>
+
+                </div>
+            </form>
+        </section>
+    );
+}
+
+export default WarehouseForm;
