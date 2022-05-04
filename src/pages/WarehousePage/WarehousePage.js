6011543175b70dcb8c48a8c1f2b4bb12193eefe4
Cyril Hahn <cyrilhahn@gmail.com>
2022-05-04 15:10:56 -0700

    responsive on all screens

--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -1,5 +1,9 @@
 import "./WarehousePage.scss";
 import React, { Component } from "react";
+import Trash from "../../assets/images/icons/delete_outline-24px.svg";
+import Pen from "../../assets/images/icons/edit-24px.svg";
+import Sort from "../../assets/images/icons/sort-24px.svg";
+import Chevron from "../../assets/images/icons/chevron_right-24px.svg";
 
 class WarehousePage extends Component {
   state = {
@@ -28,6 +32,66 @@ class WarehousePage extends Component {
               </button>
             </div>
           </div>
+        </div>
+        <div className="warehouses__card-wrapper">
+          <article className="warehouses__sort-bar warehouses__card">
+            <div className="warehouses__city-container">
+              <p className="warehouses__alt-text"> WAREHOUSE</p>
+              <img src={Sort} alt="" className="warehouses__sort-icon" />
+            </div>
+            <div className="warehouses__address-container">
+              <p className="warehouses__alt-text">ADDRESS</p>
+              <img src={Sort} alt="" className="warehouses__sort-icon" />
+            </div>
+            <div className="warehouses__name-container">
+              <p className="warehouses__alt-text">CONTACT NAME</p>
+              <img src={Sort} alt="" className="warehouses__sort-icon" />
+            </div>
+            <div className="warehouses__info-container">
+              <p className="warehouses__alt-text">CONTACT INFORMATION</p>
+              <img src={Sort} alt="" className="warehouses__sort-icon" />
+            </div>
+            <div className="warehouses__btn-container">
+              <p className="warehouses__alt-text">ACTIONS</p>
+            </div>
+          </article>
+        </div>
+        <div className="warehouses__card-wrapper">
+          <article className="warehouses__card">
+            {/* <div className="warehouses__mobile-helper"> */}
+            <div className="warehouses__city-container">
+              <p className="warehouses__alt-text--mobile">WAREHOUSE</p>
+              <div className="warehouses__chevron-container">
+                <p className="warehouses__link warehouses__copy">Manhattan</p>
+                <img src={Chevron} alt="" />
+              </div>
+            </div>
+            <div className="warehouses__address-container">
+              <p className="warehouses__alt-text--mobile">ADDRESS</p>
+              <p className="warehouses__copy">
+                503 Broadway,
+                <br className="warehouses__break"/> NewYork, USA
+              </p>
+            </div>
+            <div className="warehouses__name-container">
+              <p className="warehouses__alt-text--mobile">CONTACT NAME</p>
+              <p className="warehouses__copy">Parmin Aujla</p>
+            </div>
+            <div className="warehouses__info-container">
+              <p className="warehouses__alt-text--mobile">
+                CONTACT INFORMATION
+              </p>
+              <p className="warehouses__copy">
+                666 666 66 66 <br className="warehouses__break"/>
+                asdf@gmail.com
+              </p>
+            </div>
+            {/* </div> */}
+            <div className="warehouses__btn-container">
+              <img src={Trash} alt="" className="warehouses__icon" />
+              <img src={Pen} alt="" className="warehouses__icon" />
+            </div>
+          </article>
         </div>
       </div>
     );
