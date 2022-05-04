302d14d0dd66f55fbbe406a51081fffb9f078f99
jacky-ui <jacky.du.004@gmail.com>
2022-05-04 18:45:13 -0400

    reformat App.js

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Header />
-        {/*<Switch>
-          <Route path="/" exact component={HomePage} />*/}
-          <Route path="/warehouse/:warehouseId" component={WarehouseIndividual} />
-          {/*<Route
+        <DeleteWarehouse/>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/warehouse/:warehouseId" component={Warehouse} />
+          <Route
             path="/warehouse/:warehouseId/edit"
             component={EditWarehouse}
-          />*/}
-          {/*<Route path="/warehouse/add" component={AddWarehouse} />
+          />
+          <Route path="/warehouse/add" component={AddWarehouse} />
           <Route path="/inventory" component={Inventory} />
           <Route path="/inventory/:itemId" component={Item} />
           <Route path="/inventory/:itemId/edit" component={EditItem} />
           <Route path="/inventory/add" component={AddItem} />
-  </Switch>
-        <Footer />*/}
+        </Switch>
+        <Footer />
       </BrowserRouter>
     </div>
   );
