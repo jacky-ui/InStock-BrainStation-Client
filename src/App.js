66fc0992a02aa069ae1d1cf3fb79923666073033
ocki982 <maiderocta@gmail.com>
2022-05-05 16:46:26 -0700

    resolved some merge conflicts on App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-// import WarehouseIndividual from "./components/WarehouseIndividual/WarehouseIndividual";
+import DeleteWarehouse from "./components/DeleteWarehouse/DeleteWarehouse";
 import ItemDetailsPage from "./pages/ItemDetailsPage/ItemDetailsPage";
 
 function App() {
@@ -11,18 +11,19 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Header />
+        <DeleteWarehouse/>
         <Switch>
-          {/*<Route path="/" exact component={HomePage} />*/}
-          {/* <Route path="/warehouse/:warehouseId" component={WarehouseIndividual} /> */}
-          {/*<Route
+          <Route path="/" exact component={HomePage} />
+          <Route path="/warehouse/:warehouseId" component={Warehouse} />
+          <Route
             path="/warehouse/:warehouseId/edit"
             component={EditWarehouse}
           />
           <Route path="/warehouse/add" component={AddWarehouse} />
-          <Route path="/inventory" component={Inventory} />*/}
+          <Route path="/inventory" component={Inventory} />
           <Route path="/inventory/:itemId" component={ItemDetailsPage} />
-          {/*<Route path="/inventory/:itemId/edit" component={EditItem} />
-          <Route path="/inventory/add" component={AddItem} />*/}
+          <Route path="/inventory/:itemId/edit" component={EditItem} />
+          <Route path="/inventory/add" component={AddItem} />
         </Switch>
         <Footer />
       </BrowserRouter>
