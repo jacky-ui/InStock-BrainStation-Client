768b4e23e075e310b9d52fa6b557a8ff83f0636d
ocki982 <maiderocta@gmail.com>
2022-05-04 23:56:21 -0700

    created item details page at mobile breakpoint

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import DeleteWarehouse from "./components/DeleteWarehouse/DeleteWarehouse";
-
+// import WarehouseIndividual from "./components/WarehouseIndividual/WarehouseIndividual";
+import ItemDetailsPage from "./pages/ItemDetailsPage/ItemDetailsPage";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Header />
-        <DeleteWarehouse/>
         <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/warehouse/:warehouseId" component={Warehouse} />
-          <Route
+          {/*<Route path="/" exact component={HomePage} />*/}
+          {/* <Route path="/warehouse/:warehouseId" component={WarehouseIndividual} /> */}
+          {/*<Route
             path="/warehouse/:warehouseId/edit"
             component={EditWarehouse}
           />
           <Route path="/warehouse/add" component={AddWarehouse} />
-          <Route path="/inventory" component={Inventory} />
-          <Route path="/inventory/:itemId" component={Item} />
-          <Route path="/inventory/:itemId/edit" component={EditItem} />
-          <Route path="/inventory/add" component={AddItem} />
+          <Route path="/inventory" component={Inventory} />*/}
+          <Route path="/inventory/:itemId" component={ItemDetailsPage} />
+          {/*<Route path="/inventory/:itemId/edit" component={EditItem} />
+          <Route path="/inventory/add" component={AddItem} />*/}
         </Switch>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/ItemDetailsPage/ItemDetailsPage.js
@@ -0,0 +1,53 @@
+import "./ItemDetailsPage.scss"
+import React from 'react';
+import { Link } from "react-router-dom";
+import editIconWhite from "../../assets/images/icons/edit-24px(2).svg";
+import previousIcon from "../../assets/images/icons/arrow_back-24px.svg";
+
+const ItemDetailsPage = () => {
+    return (
+        <div>
+            <article className="subheader">
+            {/*Warehouse heading*/}
+                <section className="subheader__head">
+                    <Link className="subheader__link" to="/inventory">
+                        <img alt="previous" className="subheader__previous" src={previousIcon}/>
+                    </Link>
+                    <h1 className="subheader__head--title">Television</h1>
+                    <Link className="subheader__link" to="/inventory/:itemId/edit">
+                        <div className="subheader__head--circle">
+                            <img alt="edit" className="subheader__edit" src={editIconWhite}/>
+                            <span className="subheader__edit--text">EDIT</span>
+                        </div>
+                    </Link>
+                </section>
+                <section className="details">
+                    <div className="details__tab">
+                        <h3 className="details__title">ITEM DESCRIPTION:</h3>
+                        <p className="details__text">This 50", 4K LED TV provides a crystal-clear picture and vivid colors.</p>
+                    </div>
+                    <div className="details__tab">
+                        <h3 className="details__title">CATEGORY:</h3>
+                        <p className="details__text">Electronics</p>
+                    </div>
+                    <div className="details__flex">
+                        <div className="details__tab--space">
+                            <h3 className="details__title">STATUS:</h3>
+                            <p className="details__status">IN STOCK</p>
+                        </div>
+                        <div className="details__tab">
+                            <h3 className="details__title">QUANTITY:</h3>
+                            <p className="details__text">500</p>
+                        </div>
+                    </div>
+                    <div className="details__tab">
+                        <h3 className="details__title">WAREHOUSE:</h3>
+                        <p className="details__text">Manhattan</p>
+                    </div>
+                </section>
+            </article>
+        </div>
+    );
+};
+
+export default ItemDetailsPage;
