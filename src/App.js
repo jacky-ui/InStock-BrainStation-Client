08aaa3039294584257b7f2d85b5131342f9cbe0c
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-06 12:57:48 -0400

    Created basic file directory for delete inventory component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import "./App.scss";
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import Footer from "./components/Footer/Footer";
 import DeleteWarehouse from "./components/DeleteWarehouse/DeleteWarehouse";
 import ItemDetailsPage from "./pages/ItemDetailsPage/ItemDetailsPage";
+import WarehouseIndividual from "./components/WarehouseIndividual/WarehouseIndividual";
+import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
+
 
 function App() {
   return (
@@ -12,19 +16,17 @@ function App() {
       <BrowserRouter>
         <Header />
          <Switch>
-
-          {/*<Route path="/" exact component={HomePage} />*/}
-          <Route path="/warehouse/:warehouseId" component={WarehouseIndividual} />
-          {/*<Route
-
+          <Route path="/" exact component={WarehousePage} />
+          <Route path="/warehouse/:warehouseId" exact component={WarehouseIndividual} />
+          <Route
             path="/warehouse/:warehouseId/edit"
             component={EditWarehousePage}
           />
-          <Route path="/warehouse/add" component={AddWarehouse} />
-          <Route path="/inventory" component={Inventory} />
+          {/* <Route path="/warehouse/add" component={AddWarehouse} /> */}
+          {/* <Route path="/inventory" component={Inventory} /> */}
           <Route path="/inventory/:itemId" component={ItemDetailsPage} />
-          <Route path="/inventory/:itemId/edit" component={EditItem} />
-  <Route path="/inventory/add" component={AddItem} />*/}
+          {/* <Route path="/inventory/:itemId/edit" component={EditItem} /> */}
+          {/* <Route path="/inventory/add" component={AddItem} /> */}
         </Switch>
         <Footer />
       </BrowserRouter>
