0a96e5f9d909c1840648bd3e122a1f817683a069
Cyril Hahn <cyrilhahn@gmail.com>
2022-05-06 09:21:41 -0700

    warehouse list functional via map function and axios get request

--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -1,19 +1,36 @@
 import "./WarehousePage.scss";
 import React, { Component } from "react";
+import axios from "axios";
 import Trash from "../../assets/images/icons/delete_outline-24px.svg";
 import Pen from "../../assets/images/icons/edit-24px.svg";
 import Sort from "../../assets/images/icons/sort-24px.svg";
 import Chevron from "../../assets/images/icons/chevron_right-24px.svg";
 
+const getApi = 'http://localhost:8080/warehouses'
+
 class WarehousePage extends Component {
   state = {
     warehousesData: [],
   };
-  // A function to fetch Warehouses Data from Warehouses API Route (/warehouses)
-
-  // Below use Component Lifecycle Methods such as componentDidMount to grab data
+  
+  componentDidMount() {
+    axios.get(getApi)
+    .then((result) => {
+      this.setState({
+        warehousesData: result.data
+      })
+    })
+  }
 
   render() {
+    if (!this.state.warehousesData) {
+      return (
+      <section>
+            <p>Loading...</p>
+          </section>
+          )
+    }
+
     return (
       <div className="warehouses">
         <div className="warehouses__title-container-wrapper">
@@ -57,12 +74,13 @@ class WarehousePage extends Component {
           </article>
         </div>
         <div className="warehouses__card-wrapper">
-          <article className="warehouses__card">
+          {this.state.warehousesData.map((warehouse) => {
+            return (<article className="warehouses__card">
             {/* <div className="warehouses__mobile-helper"> */}
             <div className="warehouses__city-container">
               <p className="warehouses__alt-text--mobile">WAREHOUSE</p>
               <div className="warehouses__chevron-container">
-                <p className="warehouses__link warehouses__copy">Manhattan</p>
+                <p className="warehouses__link warehouses__copy">{warehouse.city}</p>
                 <img src={Chevron} alt="" />
               </div>
             </div>
@@ -91,44 +109,9 @@ class WarehousePage extends Component {
               <img src={Trash} alt="" className="warehouses__icon" />
               <img src={Pen} alt="" className="warehouses__icon" />
             </div>
-          </article>
-        </div>
-        <div className="warehouses__card-wrapper">
-          <article className="warehouses__card">
-            {/* <div className="warehouses__mobile-helper"> */}
-            <div className="warehouses__city-container">
-              <p className="warehouses__alt-text--mobile">WAREHOUSE</p>
-              <div className="warehouses__chevron-container">
-                <p className="warehouses__link warehouses__copy">Manhattan</p>
-                <img src={Chevron} alt="" />
-              </div>
-            </div>
-            <div className="warehouses__address-container">
-              <p className="warehouses__alt-text--mobile">ADDRESS</p>
-              <p className="warehouses__copy">
-                503 Broadway,
-                <br className="warehouses__break"/> NewYork, USA
-              </p>
-            </div>
-            <div className="warehouses__name-container">
-              <p className="warehouses__alt-text--mobile">CONTACT NAME</p>
-              <p className="warehouses__copy">Parmin Aujla</p>
-            </div>
-            <div className="warehouses__info-container">
-              <p className="warehouses__alt-text--mobile">
-                CONTACT INFORMATION
-              </p>
-              <p className="warehouses__copy">
-                666 666 66 66 <br className="warehouses__break"/>
-                asdf@gmail.com
-              </p>
-            </div>
-            {/* </div> */}
-            <div className="warehouses__btn-container">
-              <img src={Trash} alt="" className="warehouses__icon" />
-              <img src={Pen} alt="" className="warehouses__icon" />
-            </div>
-          </article>
+          </article>)
+          })}
+          
         </div>
       </div>
     );
